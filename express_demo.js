edcf3be38e2961445f6bf2a63e09a980525f909b
tom110 <254449149@qq.com>
2019-05-29 20:54:57 +0800

    定位方式为网络定位

--- express_demo.js
+++ express_demo.js
@@ -9,14 +9,18 @@ app.get('/',function(req,res){
 })
 
 app.get('/location',function(req,res){
-	var str=exec("termux-location");
+	var convert;
+	try{
+	var str=exec("termux-location -p network");
 	//var jsonObject=JSON.parse(str.toString("utf8").trim());
-	var convert=new Convert84_baidu(JSON.parse(str.toString("utf8").trim()));
+	convert=new Convert84_baidu(JSON.parse(str.toString("utf8").trim()));
 	convert.get_baidu(function(data){
 		res.send(data);
 	});
-	
-})
+	}catch(e){
+		console.log("获取定位失败!");
+	}
+});
 
 var server=app.listen(8081,function(){
 	var host=server.address().address
